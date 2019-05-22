5c0e37fc04f721229f7215b5ca4ceb798069e14e
elementarygabys <50372609+elementarygabys@users.noreply.github.com>
2019-05-22 11:30:33 -0300

    Add files via upload

---
+++ GabiJoguinho/offline.js
@@ -0,0 +1,31 @@
+﻿{
+	"version": 1558535328,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/jogador-sheet0.png",
+		"images/botãoatirar-sheet0.png",
+		"images/botãoatirar-sheet1.png",
+		"images/bala-sheet0.png",
+		"images/setacima-sheet0.png",
+		"images/setacima-sheet1.png",
+		"images/setadireita-sheet0.png",
+		"images/setadireita-sheet1.png",
+		"images/setaesquerda-sheet0.png",
+		"images/setaesquerda-sheet1.png",
+		"images/setabaixo-sheet0.png",
+		"images/setabaixo-sheet1.png",
+		"images/background-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/enimigo-sheet0.png",
+		"images/botãodestruir-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
