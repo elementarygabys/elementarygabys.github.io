21450304c23690f657db7d0d18effcaca7773bb5
elementarygabys <50372609+elementarygabys@users.noreply.github.com>
2019-05-31 18:30:41 +0000

    Add files via upload

--- GabiJoguinho/offline.js
+++ GabiJoguinho/offline.js
@@ -1,11 +1,11 @@
 ﻿{
-	"version": 1559140963,
+	"version": 1559316237,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
 		"jquery-2.1.1.min.js",
 		"offlineClient.js",
-		"images/jogador-sheet0.png",
+		"images/astronauta-sheet0.png",
 		"images/botãoatirar-sheet0.png",
 		"images/botãoatirar-sheet1.png",
 		"images/bala-sheet0.png",
@@ -22,6 +22,8 @@
 		"images/inimigo-sheet0.png",
 		"images/botãodestruir-sheet0.png",
 		"images/sprite-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/powerup-sheet0.png",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
